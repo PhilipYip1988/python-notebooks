9c87ef0a7bbfe68c0e7c7da352afc7a3491ce157
PhilipYip1988 <56493395+PhilipYip1988@users.noreply.github.com>
2023-10-07 20:06:46 +0100

    Update

---
+++ autopep8/script1.py
@@ -0,0 +1,4 @@
+var1 = 'Hello World!'
+print(var1)
+
+    
---
+++ autopep8/script2.py
@@ -0,0 +1,17 @@
+var1= 'Hello'
+var2 ="World"
+import numpy as np
+x=np.array([0,1,2,3,4])
+y=np.array([0,2,4, 6 ,8])
+import pandas as pd
+df=pd.DataFrame({'x':x,"y":y})
+import datetime
+now=datetime.datetime(year = 2023,month=12 ,day=1)
+hour=datetime.timedelta(hours=1)
+import collections
+counts=collections.Counter([1,  2,2  ,2,3,3])
+import itertools
+cycle=itertools.cycle([1,2,3])
+import sys, os
+sys.sizeof(cycle)
+os.environ['USERPROFILE']    
