1119e9a0a2a4f948b549b938bcf6fad49ee4b649
PhilipYip1988 <56493395+PhilipYip1988@users.noreply.github.com>
2023-10-20 08:45:59 +0100

    Update

--- itertools_module/helper_module.py
+++ itertools_module/helper_module.py
@@ -1,15 +1,8 @@
-"""This module has two functions identifier_group and print_identifier group.
-
-identifier_group behaves similarly to dir but can be used to return a list of strings of identifiers by the parameter kind.
-print_identifier_group prints instead of returning a list.
-"""
-
-__version__ = '0.0.1'
 
 def identifier_group(obj, kind='all', second=object, show_unique_identifiers=False, show_only_intersection_identifiers=False):
     
-    """Group identifiers from an obj into categories defined by the parameter kind. kind can have the possible values: 
-    'all', 'datamodel_method, 'datamodel_attribute', 'error_class', 'class', 'method', 'constant', or 'attribute'.
+    """ Group identifiers from an obj into categories defined by the parameter kind. kind can have the possible values: 
+    'all', 'datamodel_method, 'datamodel_attribute', 'class', 'lower_class', 'function', 'constant', 'attribute', 'internal_attribute' or 'internal_method'.
 
     second class is an optional second class for comparison, normally a parent class. 
     
@@ -38,58 +31,71 @@ def identifier_group(obj, kind='all', second=object, show_unique_identifiers=Fal
     all_grouping = []
     datamodel_method_grouping = []
     datamodel_attribute_grouping = []
-    error_class_grouping = []
-    class_grouping = []
-    method_grouping = []
+    upper_case_class_grouping = []
+    lower_case_class_grouping = []
+    function_grouping = []
     constant_grouping = []
     attribute_grouping = []
+    internal_method_grouping = []
+    internal_attribute_grouping = []
 
     for identifier in identifiers_to_examine:
         is_method = callable(getattr(obj, identifier))
         is_class = type(getattr(obj, identifier)) == type
         is_upper = identifier[0].isupper()
         is_datamodel = identifier[0:2] == '__'
+        is_internal = ((identifier[0:1] == '_') and not is_datamodel)
 
         all_grouping.append(identifier)
         if (is_method and is_datamodel):
             datamodel_method_grouping.append(identifier)
-        if (not is_method and is_datamodel):
+        if (not is_method and is_datamodel and not is_internal):
             datamodel_attribute_grouping.append(identifier)
-        if (is_upper and is_class and not is_datamodel):
-            error_class_grouping.append(identifier)
-        if (not is_upper and is_class and not is_datamodel):
-            class_grouping.append(identifier)
-        if (is_method and not is_upper and not is_class and not is_datamodel):
-            method_grouping.append(identifier)
-        if (not is_method and is_upper and not is_datamodel):
+        if (is_upper and is_class and not is_datamodel and not is_internal):
+            upper_case_class_grouping.append(identifier)
+        if (not is_upper and is_class and not is_datamodel and not is_internal):
+            lower_case_class_grouping.append(identifier)
+        if (is_method and not is_upper and not is_class and not is_datamodel and not is_internal):
+            function_grouping.append(identifier)
+        if (not is_method and is_upper and not is_datamodel and not is_internal):
             constant_grouping.append(identifier)
-        if (not is_method and not is_upper and not is_datamodel):
+        if (not is_method and not is_upper and not is_datamodel and not is_internal):
             attribute_grouping.append(identifier)
+        if (is_method and is_internal):
+            internal_method_grouping.append(identifier)
+        if (not is_method and is_internal):
+            internal_attribute_grouping.append(identifier)
 
     if (kind == 'all'):    
-        print(all_grouping)
+        return all_grouping
     elif (kind == 'datamodel_method'):   
-        print(datamodel_method_grouping)
+        return datamodel_method_grouping
     elif (kind == 'datamodel_attribute'):   
-        print(datamodel_attribute_grouping)
-    elif (kind == 'error_class'):   
-        print(error_class_grouping)
+        return datamodel_attribute_grouping
     elif (kind == 'class'):   
-        print(class_grouping)
-    elif (kind == 'method'):   
-        print(method_grouping)    
+        return upper_case_class_grouping
+    elif (kind == 'lower_class'):   
+        return lower_case_class_grouping
+    elif (kind == 'function'):   
+        return function_grouping  
     elif (kind == 'constant'):   
-        print(constant_grouping) 
+        return constant_grouping
     elif (kind == 'attribute'):   
-        print(attribute_grouping) 
+        return attribute_grouping
+    elif (kind == 'internal_method'):   
+        return internal_method_grouping
+    elif (kind == 'attribute'):   
+        return attribute_grouping
+    elif (kind == 'internal_attribute'):   
+        return internal_attribute_grouping
     else:
-        raise(ValueError, "Invalid value for kind. Possible values are 'all', 'datamodel_method, 'datamodel_attribute', 'error_class', 'class', 'method', 'constant', or 'attribute'")
+        raise(ValueError, "Invalid value for kind. Possible values are 'all', 'datamodel_method, 'datamodel_attribute', 'class', 'lower_class', 'function', 'constant', 'attribute', 'internal_attribute' or 'internal_method'.")
     
 
 def print_identifier_group(obj, kind='all', second=object, show_unique_identifiers=False, show_only_intersection_identifiers=False):
     
-    """ Group identifiers from an obj into categories defined by the parameter kind and print. kind can have the possible values: 
-    'all', 'datamodel_method, 'datamodel_attribute', 'error_class', 'class', 'method', 'constant', or 'attribute'.
+    """Group identifiers from an obj into categories defined by the parameter kind and print. kind can have the possible values: 
+    'all', 'datamodel_method, 'datamodel_attribute', 'class', 'lower_class', 'function', 'constant', 'attribute', 'internal_attribute' or 'internal_method'.
 
     second class is an optional second class for comparison, normally a parent class. 
     
@@ -99,7 +105,7 @@ def print_identifier_group(obj, kind='all', second=object, show_unique_identifie
     These are inherited from the second class when the second class is a parent class.
 
     Returns:
-        None
+        list: list of strings corresponding to identifiers in the grouping
     """
 
     identifiers = dir(obj)
@@ -118,33 +124,40 @@ def print_identifier_group(obj, kind='all', second=object, show_unique_identifie
     all_grouping = []
     datamodel_method_grouping = []
     datamodel_attribute_grouping = []
-    error_class_grouping = []
-    class_grouping = []
-    method_grouping = []
+    upper_case_class_grouping = []
+    lower_case_class_grouping = []
+    function_grouping = []
     constant_grouping = []
     attribute_grouping = []
+    internal_method_grouping = []
+    internal_attribute_grouping = []
 
     for identifier in identifiers_to_examine:
         is_method = callable(getattr(obj, identifier))
         is_class = type(getattr(obj, identifier)) == type
         is_upper = identifier[0].isupper()
         is_datamodel = identifier[0:2] == '__'
+        is_internal = ((identifier[0:1] == '_') and not is_datamodel)
 
         all_grouping.append(identifier)
-        if (is_method and is_datamodel):
+        if (is_method and is_datamodel and not is_internal):
             datamodel_method_grouping.append(identifier)
-        if (not is_method and is_datamodel):
+        if (not is_method and is_datamodel and not is_internal):
             datamodel_attribute_grouping.append(identifier)
-        if (is_upper and is_class and not is_datamodel):
-            error_class_grouping.append(identifier)
-        if (not is_upper and is_class and not is_datamodel):
-            class_grouping.append(identifier)
-        if (is_method and not is_upper and not is_class and not is_datamodel):
-            method_grouping.append(identifier)
-        if (not is_method and is_upper and not is_datamodel):
+        if (is_upper and is_class and not is_datamodel and not is_internal):
+            upper_case_class_grouping.append(identifier)
+        if (not is_upper and is_class and not is_datamodel and not is_internal):
+            lower_case_class_grouping.append(identifier)
+        if (is_method and not is_upper and not is_class and not is_datamodel and not is_internal):
+            function_grouping.append(identifier)
+        if (not is_method and is_upper and not is_datamodel and not is_internal):
             constant_grouping.append(identifier)
-        if (not is_method and not is_upper and not is_datamodel):
+        if (not is_method and not is_upper and not is_datamodel and not is_internal):
             attribute_grouping.append(identifier)
+        if (is_method and is_internal):
+            internal_method_grouping.append(identifier)
+        if (not is_method and is_internal):
+            internal_attribute_grouping.append(identifier)
 
     if (kind == 'all'):    
         print(all_grouping)
@@ -152,15 +165,21 @@ def print_identifier_group(obj, kind='all', second=object, show_unique_identifie
         print(datamodel_method_grouping)
     elif (kind == 'datamodel_attribute'):   
         print(datamodel_attribute_grouping)
-    elif (kind == 'error_class'):   
-        print(error_class_grouping)
-    elif (kind == 'class'):   
-        print(class_grouping)
-    elif (kind == 'method'):   
-        print(method_grouping)    
+    elif (kind == 'upper_class'):   
+        print(upper_case_class_grouping)
+    elif (kind == 'lower_class'):   
+        print(lower_case_class_grouping)
+    elif (kind == 'function'):   
+        print(function_grouping) 
     elif (kind == 'constant'):   
         print(constant_grouping)
     elif (kind == 'attribute'):   
         print(attribute_grouping)
+    elif (kind == 'internal_method'):   
+        print(internal_method_grouping)
+    elif (kind == 'attribute'):   
+        print(attribute_grouping)
+    elif (kind == 'internal_attribute'):   
+        print(internal_attribute_grouping)
     else:
-        raise(ValueError, "Invalid value for kind. Possible values are 'all', 'datamodel_method, 'datamodel_attribute', 'error_class', 'class', 'method', 'constant', or 'attribute'")
+        raise(ValueError, "Invalid value for kind. Possible values are 'all', 'datamodel_method, 'datamodel_attribute', 'class', 'lower_class', 'function', 'constant', 'attribute', 'internal_attribute' or 'internal_method'.")
